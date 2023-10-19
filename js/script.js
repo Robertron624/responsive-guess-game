269630d473be76f8c8943354337ee47d66835e42
Roberto Ramirez <robert.624@hotmail.com>
2023-10-18 19:25:53 -0500

    feat: can introduce characters, not functional

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const MAX_TRIES = 6;
 
 // Game initial state
 let currentWord = getWord(words[Math.floor(Math.random() * words.length)]);
+
+console.log(currentWord)
 let currentTries = 0;
 let currentMistakes = []; // array of wrong letters
 
@@ -19,9 +21,7 @@ const userInput = document.querySelector(".user-input");
 const randomButton = document.querySelector("#random-btn");
 const reetButton = document.querySelector("#reset-btn");
 
-console.log(mistakeDots)
-console.log(wordScreen)
-
+const mistakesContainer = document.querySelector(".mistakes-container");
 
 // Game functions
 
@@ -49,6 +49,7 @@ function displayUserInputBoxes () {
     for (let i = 0; i < numberOfBoxes; i++) {
         let box = document.createElement("input");
         box.setAttribute("type", "text");
+        box.setAttribute("maxlength", "1");
         box.classList.add("box");
         userInput.appendChild(box);
     }
@@ -57,6 +58,40 @@ function displayUserInputBoxes () {
 function removeUserInputBoxes () {
     userInput.innerHTML = "";
 }
+
+function checkValidCharacter (letter) {
+    const {scrambledWord} = currentWord;
+    let isCorrect = false;
+    for (let i = 0; i < scrambledWord.length; i++) {
+        if (scrambledWord[i] === letter) {
+            isCorrect = true;
+            let letterBoxes = document.querySelectorAll(".box");
+
+            let letterBox = letterBoxes[i].value 
+
+            if(letterBox !== "") {
+                letterBoxes[i].value += letter;
+            }
+        }
+    }
+    return isCorrect;
+}
+
+function checkIfUserWon (letter) {
+    let userWon = false;
+    const {word} = currentWord;
+    let userWord = "";
+    let letterBoxes = document.querySelectorAll(".box");
+    letterBoxes.forEach(box => userWord += box.value);
+    userWord += letter;
+
+    if (userWord === word) {
+        userWon = true;
+    }
+
+    return userWon;
+}
+
 
 // start when page loads
 document.addEventListener("DOMContentLoaded", () => {
@@ -72,4 +107,38 @@ document.addEventListener("DOMContentLoaded", () => {
         displayUserInputBoxes();
     });
     reetButton.addEventListener("click", resetGame);
+
+    // Add event listeners to all input boxes and bind onKeyUp event to checkValidCharacter function
+
+    const boxes = document.querySelectorAll(".user-input .box");
+
+    boxes.forEach(box => {
+        box.addEventListener("keyup", (e) => {
+            const letter = e.target.value;
+            const isCorrect = checkValidCharacter(letter);
+            if (!isCorrect) {
+                currentTries++;
+                currentMistakes.push(letter);
+                mistakeDots[currentTries - 1].classList.add("active");
+
+                let mistakeListItem = document.createElement("li");
+
+                mistakeListItem.textContent = letter;
+
+                mistakesContainer.appendChild(mistakeListItem);
+                e.target.value = "";
+            }
+
+            if (currentTries === MAX_TRIES) {
+                alert("You lost!");
+                resetGame();
+            }
+
+            if (checkIfUserWon(letter)) {
+                alert("You won!");
+                resetGame();
+            }
+        });
+    });
+
 });
