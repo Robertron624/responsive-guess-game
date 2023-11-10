6d54ca21a44a31a61586d3c9741176a8fe8319c3
Roberto Ramirez <robert.624@hotmail.com>
2023-11-10 17:41:58 -0500

    styles: break input box when word is too large

--- js/script.js
+++ js/script.js
@@ -6,8 +6,6 @@ const MAX_TRIES = 6;
 
 // Game initial state
 let currentWord = getWord(words[Math.floor(Math.random() * words.length)]);
-
-console.log(currentWord)
 let currentTries = 0;
 let currentMistakes = []; // array of wrong letters
 
@@ -35,96 +33,7 @@ function displayWord () {
     }
 }
 
-function isKeyValid (key) {
-    // Check if key is a letter from the alphabet
-    let isValid = false;
-
-    if(key.length > 1) return isValid;
-
-    const alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-    if(alphabet.includes(key)) isValid = true;
-
-    return isValid;
-}
-
-function resetGame () {
-    currentWord = getWord(words[Math.floor(Math.random() * words.length)]);
-    currentTries = 0;
-    currentMistakes = [];
-    lettersContainer.innerHTML = "";
-    mistakeDots.forEach(dot => dot.classList.remove("active"));
-    displayWord();
-}
-
-function displayUserInputBoxes () {
-    const numberOfBoxes = currentWord.scrambledWord.length;
-    for (let i = 0; i < numberOfBoxes; i++) {
-        let box = document.createElement("input");
-        box.setAttribute("type", "text");
-        box.setAttribute("maxlength", "1");
-        box.classList.add("box");
-        userInput.appendChild(box);
-    }
-}
-
-function removeUserInputBoxes () {
-    userInput.innerHTML = "";
-}
-
-function checkValidCharacter (letter, index) {
-    const {originalWord} = currentWord;
-    let isCorrect = false;
-
-    // check if the letter introduced is in the word at the index position
-    if (originalWord[index] === letter) {
-        console.log("Letter is correct")
-        isCorrect = true;
-    }
-
-    return isCorrect;
-}
-
-function checkIfUserWon (letter) {
-    let userWon = false;
-    const {originalWord} = currentWord;
-    let userWord = "";
-    let letterBoxes = document.querySelectorAll(".box");
-    letterBoxes.forEach(box => userWord += box.value);
-    // userWord += letter;
-
-    console.log("User word: ", userWord)
-    console.log("Word: ", originalWord)
-
-    if (userWord === originalWord) {
-        userWon = true;
-        console.log("User won!")
-    }
-
-    console.log("User word: ", userWord)
-    console.log("Won: ", userWon)
-
-    return userWon;
-}
-
-
-// start when page loads
-document.addEventListener("DOMContentLoaded", () => {
-    displayWord();
-    displayUserInputBoxes();
-
-
-    randomButton.addEventListener("click", () => {
-        currentWord = getWord(words[Math.floor(Math.random() * words.length)]);
-        lettersContainer.innerHTML = "";
-        displayWord();
-        removeUserInputBoxes();
-        displayUserInputBoxes();
-    });
-    reetButton.addEventListener("click", resetGame);
-
-    // Add event listeners to all input boxes and bind onKeyUp event to checkValidCharacter function
-
+function addEventListenersToBoxes () {
     const boxes = document.querySelectorAll(".user-input .box");
 
     boxes.forEach((box, index) => {
@@ -132,7 +41,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
             if(!isKeyValid(e.key)) {
-                console.log("Invalid key")
                 e.target.value = "";
                 return;
             };
@@ -156,5 +64,91 @@ document.addEventListener("DOMContentLoaded", () => {
 
         });
     });
+}
 
+function isKeyValid (key) {
+    // Check if key is a letter from the alphabet
+    let isValid = false;
+
+    if(key.length > 1) return isValid;
+
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    if(alphabet.includes(key)) isValid = true;
+
+    return isValid;
+}
+
+function resetGame () {
+    currentWord = getWord(words[Math.floor(Math.random() * words.length)]);
+    currentTries = 0;
+    currentMistakes = [];
+    lettersContainer.innerHTML = "";
+    removeUserInputBoxes();
+    mistakeDots.forEach(dot => dot.classList.remove("active"));
+    displayWord();
+    displayUserInputBoxes();
+    addEventListenersToBoxes();
+}
+
+function displayUserInputBoxes () {
+    const numberOfBoxes = currentWord.scrambledWord.length;
+    for (let i = 0; i < numberOfBoxes; i++) {
+        let box = document.createElement("input");
+        box.setAttribute("type", "text");
+        box.setAttribute("maxlength", "1");
+        box.classList.add("box");
+        userInput.appendChild(box);
+    }
+}
+
+function removeUserInputBoxes () {
+    userInput.innerHTML = "";
+}
+
+function checkValidCharacter (letter, index) {
+    const {originalWord} = currentWord;
+    let isCorrect = false;
+
+    // check if the letter introduced is in the word at the index position
+    if (originalWord[index] === letter) {
+        isCorrect = true;
+    }
+
+    return isCorrect;
+}
+
+function checkIfUserWon (letter) {
+    let userWon = false;
+    const {originalWord} = currentWord;
+    let userWord = "";
+    let letterBoxes = document.querySelectorAll(".box");
+    letterBoxes.forEach(box => userWord += box.value);
+
+    if (userWord === originalWord) {
+        userWon = true;
+        console.log("User won!")
+    }
+
+    return userWon;
+}
+
+
+// start when page loads
+document.addEventListener("DOMContentLoaded", () => {
+    displayWord();
+    displayUserInputBoxes();
+
+
+    randomButton.addEventListener("click", () => {
+        currentWord = getWord(words[Math.floor(Math.random() * words.length)]);
+        lettersContainer.innerHTML = "";
+        displayWord();
+        removeUserInputBoxes();
+        displayUserInputBoxes();
+    });
+    reetButton.addEventListener("click", resetGame);
+
+    // Add event listeners to all input boxes and bind onKeyUp event to checkValidCharacter function
+    addEventListenersToBoxes();
 });
